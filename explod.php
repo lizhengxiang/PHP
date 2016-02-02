00db8b6dc3be10c493e980a6c6ec8943fbb93494
lizhengxiang <1065482100@qq.com>
2016-02-02 10:58:08 +0800

    xxxx

---
+++ explod.php
@@ -0,0 +1,4 @@
+<?php
+print $words = explode(' ','my senttence is not very complicated');
+print "\n";
+var_dump($words);
--- output.php
+++ output.php
@@ -5,7 +5,7 @@ $sales = array(
 	array('Norr3','2015-01-01'),
 	array('Norr4','2015-01-01')
 );
-$fh = fopen('salse.csv','w')or die("can not open salse.csv");
+$fh = fopen('php://output','w');
 foreach($sales as $sales_line){
 	if(fputcsv($fh,$sales_line) == false){
 		//die输出当前前脚本并推出程序
